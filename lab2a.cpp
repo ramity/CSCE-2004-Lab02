b474f526a621151381a00f72f6b9ae94ea330a2b
ramity <lcb005@email.uark.edu>
2016-08-30 14:08:35 -0500

    init commit

---
+++ lab2a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Main function
+int main()
+{
+   cout << "Here are some approximations of PI:" << endl;
+
+   // Archimedes 225 BC
+   cout << "22 / 7 = " << (float)22 / 7 << endl;
+
+   // Zu Chongzhi 480 AD
+   cout << "355 / 113 = " << (float)355 / 113 << endl;
+
+   // Indiana law 1897 AD
+   cout << "16 / 5 = " << (float)16 / 5 << endl;
+
+   return 0;
+}
+
